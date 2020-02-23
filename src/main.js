9741ce22368af9f1b7a0ffe6a7dfe84af7321027
liyb <liyb@stlx.com.cn>
2020-02-23 10:57:27 +0800

    底部购物车图标制作完毕

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 
 import app from "./App.vue"
 
+//导入路由包
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 //按需导入组件
 import { Header} from 'mint-ui'
 
 //导入mui
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 Vue.component(Header.name, Header)
 
